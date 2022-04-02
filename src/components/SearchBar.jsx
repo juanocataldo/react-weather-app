119bdda3b318d1f4d6b6cae9427bdc3be8c8c338
juanocataldo <juanocataldo@gmail.com>
2022-04-02 16:50:05 -0300

    search fixed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -23,7 +23,7 @@ export default function SearchBar({pushCity}){
                     <NavLink exact to="/" activeClassName={`btn btn-sm ${style.btnapp}`}>Home</NavLink>
                     <NavLink to="/about" activeClassName={`btn btn-sm ${style.btnapp}`}>About</NavLink>
                 </div>
-                <form className="d-flex">
+                <form className="d-flex" onSubmit={pushIt}>
                     <div className="col-sm-6">
                         <input type="text" placeholder="Ingresar ciudad" onChange={modInput} value={input} className="form-control form-control-sm me-2" />
 
